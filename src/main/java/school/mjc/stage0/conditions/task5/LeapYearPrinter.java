0a1c6231d0bb2664c5c8fa91238eda3e24f86407
maiko <maiko.mode5@gmail.com>
2024-05-17 23:28:53 -0400

    done

--- src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
@@ -2,5 +2,18 @@ package school.mjc.stage0.conditions.task5;
 
 public class LeapYearPrinter {
     public void isLeapYear(int year) {
+        boolean isLeap = (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+
+        switch (isLeap ? 1 : 0) {
+            case 1:
+                System.out.println("leap");
+                break;
+            case 0:
+                System.out.println("not leap");
+                break;
+            default:
+                System.out.println("Invalid");
+                break;
+        }
     }
 }
